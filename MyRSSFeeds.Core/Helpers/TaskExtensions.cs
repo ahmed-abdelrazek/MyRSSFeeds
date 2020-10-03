6883f79dbc8696bc13e6bc2930a6f3dbd8448dab
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2020-10-03 16:29:48 +0200

    Task method void handler

--- MyRSSFeeds.Core/Helpers/TaskExtensions.cs
+++ MyRSSFeeds.Core/Helpers/TaskExtensions.cs
@@ -5,10 +5,24 @@ namespace MyRSSFeeds.Core.Helpers
 {
     public static class TaskExtensions
     {
-        public static void FireAndForget(this Task task)
+        /// <summary>
+        /// Fire Task method in property or constructor 
+        /// that can handle await and exception
+        /// </summary>
+        /// <param name="task">The task to complete</param>
+        /// <param name="completedCallback">To do on complete</param>
+        /// <param name="errorCallback">To do on exception</param>
+        public async static void FireAndGet(this Task task, Action completedCallback, Action<Exception> errorCallback)
         {
-            // This method allows you to call an async method without awaiting it.
-            // Use it when you don't want or need to wait for the task to complete.
+            try
+            {
+                await task;
+                completedCallback?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                errorCallback?.Invoke(ex);
+            }
         }
     }
 }
