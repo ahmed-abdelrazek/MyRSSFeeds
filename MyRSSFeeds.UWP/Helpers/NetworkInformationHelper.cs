44a9df97e1506cd27036f1ac2fe3d182aae25074
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2020-10-04 22:44:26 +0200

    Better way to check the internet connection

---
+++ MyRSSFeeds.UWP/Helpers/NetworkInformationHelper.cs
@@ -0,0 +1,28 @@
+﻿using Windows.Networking.Connectivity;
+
+namespace MyRSSFeeds.Helpers
+{
+    public class NetworkInformationHelper
+    {
+        public bool HasInternetAccess { get; private set; }
+
+        public NetworkInformationHelper()
+        {
+            NetworkInformation.NetworkStatusChanged += NetworkInformationOnNetworkStatusChanged;
+            CheckInternetAccess();
+        }
+
+        private void NetworkInformationOnNetworkStatusChanged(object sender)
+        {
+            CheckInternetAccess();
+        }
+
+        private void CheckInternetAccess()
+        {
+            var connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            HasInternetAccess = (connectionProfile != null &&
+                                 connectionProfile.GetNetworkConnectivityLevel() ==
+                                 NetworkConnectivityLevel.InternetAccess);
+        }
+    }
+}
