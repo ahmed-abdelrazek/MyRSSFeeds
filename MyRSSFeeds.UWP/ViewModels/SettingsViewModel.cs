c2a72d939c1e0c0aaa826e9c4d9c4ebf888f9e5a
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2020-10-13 19:41:32 +0200

    Import/Export sources as json

--- MyRSSFeeds.UWP/ViewModels/SettingsViewModel.cs
+++ MyRSSFeeds.UWP/ViewModels/SettingsViewModel.cs
@@ -1,9 +1,14 @@
-﻿using MyRSSFeeds.Helpers;
+﻿using MyRSSFeeds.Core.Models;
+using MyRSSFeeds.Helpers;
 using MyRSSFeeds.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.ApplicationModel;
 using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 
 namespace MyRSSFeeds.ViewModels
@@ -61,6 +66,124 @@ namespace MyRSSFeeds.ViewModels
                 }
 
                 return _switchThemeCommand;
+            }
+        }
+
+        private ICommand _exportSourceAsJsonCommand;
+
+        public ICommand ExportSourceAsJsonCommand
+        {
+            get
+            {
+                if (_exportSourceAsJsonCommand == null)
+                {
+                    _exportSourceAsJsonCommand = new RelayCommand(
+                        async () =>
+                        {
+                            var savePicker = new Windows.Storage.Pickers.FileSavePicker
+                            {
+                                SuggestedStartLocation =
+                                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
+                            };
+                            // Dropdown of file types the user can save the file as
+                            savePicker.FileTypeChoices.Add("JSON File", new List<string>() { ".json" });
+                            // Default file name if the user does not type one in or select a file to replace
+                            savePicker.SuggestedFileName = "ExportedRssSources";
+                            StorageFile file = await savePicker.PickSaveFileAsync();
+                            if (file != null)
+                            {
+                                try
+                                {
+                                    // Prevent updates to the remote version of the file until
+                                    // we finish making changes and call CompleteUpdatesAsync.
+                                    CachedFileManager.DeferUpdates(file);
+                                    // write to file
+                                    var jsonContent = await Core.Helpers.Json.StringifyAsync(await Core.Services.SourceDataService.GetSourcesDataAsync());
+                                    await FileIO.WriteTextAsync(file, jsonContent);
+                                    // Let Windows know that we're finished changing the file so
+                                    // the other app can update the remote version of the file.
+                                    // Completing updates may require Windows to ask for user input.
+                                    Windows.Storage.Provider.FileUpdateStatus status =
+                                        await CachedFileManager.CompleteUpdatesAsync(file);
+                                    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                                    {
+                                        await new MessageDialog(string.Format("Settings_ExportSuccessfulMessageDialog".GetLocalized(), file.Name)).ShowAsync();
+                                    }
+                                    else
+                                    {
+                                        await new MessageDialog(string.Format("Settings_ExportFailedMessageDialog".GetLocalized(), file.Name)).ShowAsync();
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Debug.WriteLine(ex);
+                                    await new MessageDialog(string.Format("Settings_ExportErrorMessageDialog".GetLocalized(), file.Name)).ShowAsync();
+                                }
+                            }
+                            else
+                            {
+                                await new MessageDialog("Settings_ExportCanceledMessageDialog".GetLocalized()).ShowAsync();
+                            }
+                        });
+                }
+
+                return _exportSourceAsJsonCommand;
+            }
+        }
+
+        private ICommand _importSourceAsJsonCommand;
+
+        public ICommand ImportSourceAsJsonCommand
+        {
+            get
+            {
+                if (_importSourceAsJsonCommand == null)
+                {
+                    _importSourceAsJsonCommand = new RelayCommand(
+                        async () =>
+                        {
+                            var picker = new Windows.Storage.Pickers.FileOpenPicker
+                            {
+                                ViewMode = Windows.Storage.Pickers.PickerViewMode.List,
+                                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
+                            };
+                            picker.FileTypeFilter.Add(".json");
+
+                            StorageFile file = await picker.PickSingleFileAsync();
+                            if (file != null)
+                            {
+                                try
+                                {
+                                    // Application now has read/write access to the picked file
+                                    var sourcesList = await Core.Helpers.Json.ToObjectAsync<List<Source>>(await FileIO.ReadTextAsync(file));
+
+                                    foreach (var source in sourcesList)
+                                    {
+                                        if (await Core.Services.SourceDataService.SourceExistAsync(source.RssUrl.ToString()))
+                                        {
+                                            continue;
+                                        }
+                                        else
+                                        {
+                                            await Core.Services.SourceDataService.AddNewSourceAsync(source);
+                                        }
+                                    }
+                                    await new MessageDialog(string.Format("Settings_ImportSuccessfulMessageDialog".GetLocalized(), file.Name)).ShowAsync();
+                                }
+                                catch (Exception ex)
+                                {
+                                    Debug.WriteLine(ex);
+                                    await new MessageDialog(string.Format("Settings_ImportErrorMessageDialog".GetLocalized(), file.Name)).ShowAsync();
+                                }
+                            }
+                            else
+                            {
+                                await new MessageDialog("Settings_ImportCanceledMessageDialog".GetLocalized()).ShowAsync();
+                            }
+                        });
+                }
+
+                return _importSourceAsJsonCommand;
             }
         }
 
