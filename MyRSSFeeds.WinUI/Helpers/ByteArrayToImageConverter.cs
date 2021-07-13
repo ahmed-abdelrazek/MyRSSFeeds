c35381f11e47d7ea5da496bcd523a67e11bf8a1f
Ahmed Mohamed Abdel-Razek <ahmed_master23@hotmail.com>
2021-07-13 06:30:39 +0200

    Add ByteArray To Image Converter

---
+++ MyRSSFeeds.WinUI/Helpers/ByteArrayToImageConverter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using Windows.Storage.Streams;
+
+namespace MyRSSFeeds.Helpers
+{
+    public class ByteArrayToImageConverter : IValueConverter
+    {
+        public ByteArrayToImageConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is byte[] ba && ba is not null)
+            {
+                using InMemoryRandomAccessStream stream = new();
+                using (DataWriter writer = new(stream.GetOutputStreamAt(0)))
+                {
+                    writer.WriteBytes(ba);
+                    writer.StoreAsync().GetResults();
+                }
+                BitmapImage image = new();
+                image.SetSource(stream);
+                return image;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
