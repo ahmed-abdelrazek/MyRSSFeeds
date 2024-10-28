7c228242e8801377ecafaee99b2e70710df0675b
Ahmed Abdel-Razek <ahmed_master23@hotmail.com>
2024-10-28 19:30:09 +0300

    Add OPMLCore.NET Project

---
+++ OPMLCore.NET/Body.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+// from https://github.com/fnya/OPMLCore.NET with MIT License 2018
+
+namespace OPMLCore.NET
+{
+    public class Body
+    {
+        ///<summary>
+        /// Outline list
+        ///</summary>
+        public List<Outline> Outlines { get; set; } = new List<Outline>();
+
+        ///<summary>
+        /// Constructor
+        ///</summary>
+        public Body()
+        {
+
+        }
+
+        ///<summary>
+        /// Constructor
+        ///</summary>
+        /// <param name="element">element of Body</param>
+        public Body(XmlElement element)
+        {
+            if (element.Name.Equals("body", StringComparison.CurrentCultureIgnoreCase))
+            {
+                foreach (XmlNode node in element.ChildNodes)
+                {
+                    if (node.Name.Equals("outline", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        Outlines.Add(new Outline((XmlElement)node));
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder buf = new StringBuilder();
+            buf.Append("<body>\r\n");
+            foreach (Outline outline in Outlines)
+            {
+                buf.Append(outline.ToString());
+            }
+            buf.Append("</body>\r\n");
+
+            return buf.ToString();
+        }
+    }
+}
