b1b037da9e3b133b2a281f4e34795b2ab8de605a
Ahmed Abdel-Razek <ahmed_master23@hotmail.com>
2024-10-29 17:34:09 +0300

    Update Assembly Copyright

--- MyRSSFeeds.UWP/Properties/AssemblyInfo.cs
+++ MyRSSFeeds.UWP/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ShababConquer")]
 [assembly: AssemblyProduct("MyRSSFeeds")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
